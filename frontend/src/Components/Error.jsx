de867a7fb385e5b690971c8c4b243aeea2b222c1
AlolaSunday12 <alolasj@gmail.com>
2024-04-01 11:08:43 +0100

    final touch on components

--- frontend/src/Components/Error.jsx
+++ frontend/src/Components/Error.jsx
@@ -12,7 +12,7 @@ function Error({message}) {
 }
 
 Error.propTypes = {
-    message: PropTypes.string.isRequired, // Define message prop as a required string
+    message: PropTypes.string.isRequired, 
 };
 
 export default Error;
