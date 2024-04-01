0c2e6dc75b28a09e4b2d23b4b203f4cc45e875fa
AlolaSunday12 <alolasj@gmail.com>
2024-04-01 11:22:14 +0100

    cleanup

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,6 +1,6 @@
-import React, {useContext,  useEffect, useState } from 'react';
+import {useContext } from 'react';
 import './App.css';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { userContext } from './App.jsx';
 import axios from 'axios';
 
